4d0b3b4c97fca1b34bb17d2dc552036748bdcf89
Alessandro Calista <alessandromaria.calista@gmail.com>
2026-06-29 09:28:19 +0200

    feat: changed populate logic to support pdf files using markitdown

--- populate.py
+++ populate.py
@@ -1,29 +1,48 @@
 import os
 
+from markitdown import MarkItDown
+
 import service.database as db
 from service.embedding.chunk import extract_chunks
 from service.embedding.embedder import embed_chunks
 
-ACCEPTED_EXTENSIONS = [".txt", ".md", ".log"]
+ACCEPTED_EXTENSIONS = ["txt", "md", "log", "pdf"]
 DATA_DIR = "data/workset"
+MAX_WORDS = 400
+
+
+def extract_from_plain_file(path: str) -> str:
+    with open(path, "r", encoding="utf-8") as f:
+        content = f.read()
+        return content
+
+
+def extract_from_pdf(path: str) -> str:
+    mid = MarkItDown()
+    md = mid.convert(source=path).markdown
+    return md
 
 
 def main():
     for file in os.listdir(DATA_DIR):
-        ext = os.path.splitext(file)[1]
+        ext = file.split(".")[-1]
+        path = DATA_DIR + "/" + file
         if ext not in ACCEPTED_EXTENSIONS:
             continue
 
-        with open(DATA_DIR + "/" + file, "r", encoding="utf-8") as f:
-            print("Embedding: " + file)
-            content = f.read()
-            content_chunks = extract_chunks(content, delimiter="\n\n")
+        print(f"Trying to extract from {file} ext {ext}")
 
-            print(f"Total chunks: {len(content_chunks)}")
-            embeddings = embed_chunks(content_chunks)
+        content = (
+            extract_from_pdf(path) if ext == "pdf" else extract_from_plain_file(path)
+        )
 
-            db.batch_insert_embeddings(embeddings)
-            print(f"Inserted {len(embeddings)} embeddings for {file}")
+        content_chunks = extract_chunks(content, max_words=MAX_WORDS)
+
+        print(f"Total chunks: {len(content_chunks)}")
+        embeddings = embed_chunks(content_chunks)
+
+        db.batch_insert_embeddings(embeddings)
+        print(f"Inserted {len(embeddings)} embeddings for {file}")
 
 
 if __name__ == "__main__":
