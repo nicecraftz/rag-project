0ecddd42f236264fe067902553370bab2ce51fc9
Alessandro Calista <alessandromaria.calista@gmail.com>
2026-06-30 15:05:48 +0200

    chore: removed unused env from env_provider

--- service/env_provider.py
+++ service/env_provider.py
@@ -6,7 +6,6 @@ dotenv.load_dotenv()
 
 EMBEDDING_MODEL = os.getenv("OLLAMA_EMBEDDING_MODEL", "qwen3-embedding:0.6b")
 ENRICHMENT_MODEL = os.getenv("OLLAMA_ENRICHMENT_MODEL", "qwen3:8b")
-CHUNK_MAX_WORDS = int(os.getenv("CHUNK_MAX_WORDS", 100))
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "default_password")
